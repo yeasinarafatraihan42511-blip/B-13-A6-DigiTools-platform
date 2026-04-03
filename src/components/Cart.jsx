2f3548fa757f8b6941758f3c268b5f4e7530c44c
Yeasin <yeasinarafatraihan42511@gmail.com>
2026-04-03 09:24:26 +0600

    proched btn done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Cart = ({ carts }) => {
-// const totalPrice = carts.reduce((sum,item) => sum + item.price, 0) 
+const totalPrice = carts.reduce((sum,item) => sum + item.price, 0) 
+console.log(totalPrice)
     return (
         <div className=" p-10 max-w-7xl mx-auto ">
             <h1 className='font-bold text-black'>Your Cart</h1>
@@ -29,9 +30,9 @@ const Cart = ({ carts }) => {
             <div className='py-5 border-rounded-lg p-3 flex justify-between items-center'>
                 {/* <div className='font-bold text-black'>Total: ${carts.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2)}</div> */}
                 <div>
-                    <h3 className='font-black font-bold text-2xl'>Total:</h3>
+                    <h3 className='font-black font-bold text-3xl'>Total:</h3>
                 </div>
-                <div className='font-bold text-black'>${carts.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2)}</div>
+                <div className='font-bold text-3xl text-black'>${totalPrice}</div>
             </div>
             <div>
                 <button className='btn btn-primary text-white rounded-3xl w-full'>Proceed to Checkout</button>
